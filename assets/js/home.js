16bd1b40a457644481f2206073a774a4290e6f6c
heera.ahn <hi.heeraahn@gmail.com>
2022-11-26 04:18:30 +0900

    1126디자인변경

---
+++ assets/js/home.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  //animation_rolling();
+}
+
+// function animation_rolling(t){
+//   let list = document.getElementsByClassName('rolling_txt')[0];
+//   let temp = list.cloneNode(true);
+//   list.parentNode.appendChild(temp);
+// }
+
+
